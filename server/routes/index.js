92601ca7a0daf3192b51a16f7a6b95756039a780
KonggMinhh <hoangkongminh2001@gmail.com>
2024-04-24 02:12:15 +0700

    hash password

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,9 @@
 const userRouter = require("./user");
+const { notFound, errHandler } = require("../middlewares/errorHandler");
 const initRouter = (app) => {
     app.use("/api/users", userRouter);
+    app.use(notFound);
+    app.use(errHandler);
 };
 
 module.exports = initRouter;
