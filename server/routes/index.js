d51a4a39223ced2794cedb064c4b1c190d25b0cb
KonggMinhh <hoangkongminh2001@gmail.com>
2024-04-26 19:49:52 +0700

    product

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,10 @@
 const userRouter = require("./user");
 const { notFound, errHandler } = require("../middlewares/errorHandler");
+
 const initRouter = (app) => {
-    app.use("/api/users", userRouter);
+    app.use("/api/user", userRouter);
     app.use(notFound);
     app.use(errHandler);
 };
 
 module.exports = initRouter;
-
-
-
-
-
-
